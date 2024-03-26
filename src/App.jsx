9363092ec57a5a42fc8361349be97b11a2636228
Kritinitya7 <kritinitya7@gmail.com>
2024-03-26 22:19:30 +0530

    logical operator

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "./App.css";
 // import React from "react";
 
 function App() {
-  let foodItems = ["Dal", "Rice", "Pulao", "Biryani"];
-  let emptyMessage =
-    foodItems.length == 0 ? <h3>I am still Hungary.</h3> : null;
+  let foodItems = [];
+  // let foodItems = ["Dal", "Rice", "Pulao", "Biryani"];
+
   return (
     <>
       <h1>Healthy Food</h1>
-      {emptyMessage}
+      {foodItems.length == 0 && <h3>I am still Hungary.</h3>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
