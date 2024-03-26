4bdf863afc951ffcf461c62a806d486cef28a704
Kritinitya7 <kritinitya7@gmail.com>
2024-03-26 21:44:18 +0530

    Ternary Operators

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import "./App.css";
 
 function App() {
   let foodItems = ["Dal", "Rice", "Pulao", "Biryani"];
+  let emptyMessage =
+    foodItems.length == 0 ? <h3>I am still Hungary.</h3> : null;
   return (
     <>
       <h1>Healthy Food</h1>
+      {emptyMessage}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
